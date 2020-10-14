9178b55adda2f7bd77a8181e135a9d6128fa82a4
Junjie Li <jjtommyliii@gmail.com>
2020-10-15 00:59:18 +0200

    Implement BaseFragment and BaseViewModel

--- app/src/main/java/com/example/marvelcharacters/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/marvelcharacters/ui/base/BaseFragment.kt
@@ -1,4 +1,70 @@
 package com.example.marvelcharacters.ui.base
 
-class BaseFragmnet {
+import android.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.example.marvelcharacters.R
+import com.example.marvelcharacters.hideKeyboard
+import org.koin.android.viewmodel.ext.android.viewModelByClass
+import kotlin.reflect.KClass
+
+abstract class BaseFragment<VM: BaseViewModel>: Fragment() {
+
+    protected val viewModel: VM by viewModelByClass(this.getViewModel())
+
+
+    abstract fun getLayout() : Int
+    abstract fun getViewModel() : KClass<VM>
+
+    private var progressDialog: AlertDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel.loadingLD.observe(this, Observer {
+            if (it) showLoading()
+            else hideLoading()
+        })
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(getLayout(),container,false)
+    }
+
+    private fun Fragment.createLoadingDialog(): AlertDialog?{
+        return this.activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setView(R.layout.fullscreen_loading_dialog)
+            builder.setCancelable(false)
+            builder.create().apply {
+                window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            }
+        }
+    }
+
+    private fun showLoading(){
+        if (progressDialog == null){
+            progressDialog = createLoadingDialog()
+            progressDialog?.show()
+            view?.hideKeyboard()
+        }
+    }
+
+    private fun hideLoading(){
+        progressDialog?.dismiss()
+        progressDialog = null
+    }
+
+    fun toast( message: String, duration: Int = Toast.LENGTH_LONG)=
+        Toast.makeText(context, message , duration).show()
+
+
+
 }
--- app/src/main/java/com/example/marvelcharacters/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/example/marvelcharacters/ui/base/BaseViewModel.kt
@@ -1,2 +1,11 @@
 package com.example.marvelcharacters.ui.base
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+
+abstract class BaseViewModel (application: Application):AndroidViewModel(application){
+
+    val loadingLD = MutableLiveData<Boolean>()
+
+}
