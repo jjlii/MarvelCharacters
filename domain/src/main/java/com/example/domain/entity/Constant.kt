ff3b4bf50dd710b0a2adf04acad9b36fe3a90b8f
Junjie Li <jjtommyliii@gmail.com>
2020-10-18 15:09:49 +0200

    Unit Test

--- domain/src/main/java/com/example/domain/entity/Constant.kt
+++ domain/src/main/java/com/example/domain/entity/Constant.kt
@@ -2,7 +2,7 @@ package com.example.domain.entity
 
 class Constant {
     object ConnectionUtils{
-        const val PUB_KEY = "c5ec6a0c72b1e3afc92267da3915d951"
-        const val PV_KEY = "adb53869c97301ee7ed1897ae6d850cbd8b8d388"
+        const val PUB_KEY = "PUB_KEY"
+        const val PV_KEY = "PV_KEY"
     }
 }
